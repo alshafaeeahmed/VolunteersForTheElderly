7d649c20c2815c8ee8bb535b2154ab8c01fdd7bc
alshafaeeahmed <alshafaeeahmed@gmail.com>
2020-12-19 12:22:43 -0800

    edit profile.html

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
-from phone_field import PhoneField
 
 
 class Profile(models.Model):
