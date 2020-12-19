ccbc0dc913d7529bfcd728bdc94f5fa0716b8e07
alshafaeeahmed <alshafaeeahmed@gmail.com>
2020-12-19 00:09:05 -0800

    fix register form

--- VolunteersForTheElderly/urls.py
+++ VolunteersForTheElderly/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views as user_views
 from django.contrib.auth.decorators import login_required
@@ -7,22 +7,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
-
 urlpatterns = [
     path('', user_views.feed, name='home'),
     path('admin/', admin.site.urls),
-    path('core/',include('core.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
-     path('welcome/',user_views.welcome,name="welcome"),
-     path('logout/',auth_views.LogoutView.as_view(template_name='core/logout.html'),name='logout'),
-     path('register/',user_views.UserFormView.as_view(template_name='core/registration_form.html'),name='register'),
-     url(r'^profile/(?P<username>\w+)/$',user_views.profile,name='profile'),
-     url(r'^followweb/(?P<username>\w+)/$',user_views.followweb,name="followweb"),
-     url(r'^unfollowweb/(?P<username>\w+)/$',user_views.unfollowweb,name="unfollowweb"),
-     url(r'^postweb/(?P<username>\w+)/$',user_views.postweb,name="postweb"),
-     url(r'^commentweb/(?P<username>\w+)/(?P<post_id>\d+)/$', user_views.commentweb,name = "commentweb"),
-     path('feed/',user_views.feed,name="feed"),
-     
+    path('core/', include('core.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('welcome/', user_views.welcome, name="welcome"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+    path('register/', user_views.UserFormView.as_view(template_name='core/registration_form.html'), name='register'),
+    url(r'^profile/(?P<username>\w+)/$', user_views.profile, name='profile'),
+    url(r'^followweb/(?P<username>\w+)/$', user_views.followweb, name="followweb"),
+    url(r'^unfollowweb/(?P<username>\w+)/$', user_views.unfollowweb, name="unfollowweb"),
+    url(r'^postweb/(?P<username>\w+)/$', user_views.postweb, name="postweb"),
+    url(r'^commentweb/(?P<username>\w+)/(?P<post_id>\d+)/$', user_views.commentweb, name="commentweb"),
+    path('feed/', user_views.feed, name="feed"),
+
 ]
 
 if settings.DEBUG:
