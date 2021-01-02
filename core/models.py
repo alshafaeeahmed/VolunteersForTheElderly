2a0af1d9938147ec7adeb54abfe8817cae4e7f4b
alshafaeeahmed <alshafaeeahmed@gmail.com>
2021-01-02 13:52:47 -0800

    Add gender and address for profiles

--- core/models.py
+++ core/models.py
@@ -2,6 +2,15 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
 
+ADDRESS_CHOICES = (
+    ('BeerSheba', 'BEERSHEBA'),
+    ('TelAviv', 'TELAVIV'),
+)
+GENDER_CHOICES = (
+    ('Male', 'MALE'),
+    ('Female', 'FEMALE'),
+)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,8 +20,8 @@ class Profile(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,10}$',
                                  message="Phone number must be entered in the format: '+0000000000'")
     phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True)
-
-
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='Male')
+    address = models.CharField(max_length=10, choices=ADDRESS_CHOICES, default='BeerSheba')
 
     def __str__(self):
         return f'{self.user.username} Profile'
