4828d154a5ea4ec91fcabf99ebbb5c9fac2cc4f9
salem <salemgu@ac.sce.ac.il>
2021-01-04 12:53:56 +0200

    Adding Feedback Model to Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
+from . import models
 from .models import Profile, Following, Follower, Post, Comment
+
+
+# Adding Feedback Model to Django Admin
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'date',)
+    search_fields = ('name', 'email',)
+    date_hierarchy = 'date'
+
 
 admin.site.register(Profile)
 admin.site.register(Follower)
 admin.site.register(Following)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(models.Feedback, FeedbackAdmin)
